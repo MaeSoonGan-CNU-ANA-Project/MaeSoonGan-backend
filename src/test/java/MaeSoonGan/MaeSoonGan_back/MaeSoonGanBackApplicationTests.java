34b3085faf466c8c2258734cdb1fbdbf1c4de0a9
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2025-03-09 16:21:37 +0900

    Chore: set project

---
+++ src/test/java/MaeSoonGan/MaeSoonGan_back/MaeSoonGanBackApplicationTests.java
@@ -0,0 +1,13 @@
+package MaeSoonGan.MaeSoonGan_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaeSoonGanBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
